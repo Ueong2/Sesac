82ae60b73f1fe9d10f3a6df880fe197ada2f8dd6
Ueong2 <107999594+Ueong2@users.noreply.github.com>
2023-10-30 00:52:52 +0900

    스프링 학습

---
+++ step01di/src/main/java/dev/spring/step02factory/Tape.java
@@ -0,0 +1,26 @@
+package dev.spring.step02factory;
+
+public class Tape {
+	String name; // 테이프 이름
+	boolean isWorked; // 정상동작 여부
+	
+	
+	public Tape(String name, boolean isWorked) {
+		super();
+		this.name = name;
+		this.isWorked = isWorked;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isWorked() {
+		return isWorked;
+	}
+	public void setWorked(boolean isWorked) {
+		this.isWorked = isWorked;
+	}
+
+}
